7cfad214ee4cadd24063ded4dd3f4c181aae6d20
l-calax-l <salikhovilyas228@gmail.com>
2025-10-28 21:46:02 +0400

    refactor: implement Page Object Model for manager page

---
+++ pages/manager_page.py
@@ -0,0 +1,63 @@
+import allure
+from selenium.webdriver.common.by import By
+from .base_page import BasePage
+
+class ManagerPageLocators:
+    # --- Локаторы кнопок-вкладок ---
+    ADD_CUSTOMER_BUTTON = (By.CSS_SELECTOR, "button[ng-click='addCust()']")
+    CUSTOMERS_BUTTON = (By.CSS_SELECTOR, "button[ng-click='showCust()']")
+
+    # --- Локаторы полей формы Add Customer ---
+    FIRST_NAME_INPUT = (By.CSS_SELECTOR, "input[ng-model='fName']")
+    LAST_NAME_INPUT = (By.CSS_SELECTOR, "input[ng-model='lName']")
+    POST_CODE_INPUT = (By.CSS_SELECTOR, "input[ng-model='postCd']")
+    SUBMIT_CUSTOMER_BUTTON = (By.CSS_SELECTOR, "button[type='submit']")
+
+    # --- Локаторы на вкладке Customers ---
+    SEARCH_INPUT = (By.CSS_SELECTOR, "input[ng-model='searchCustomer']")
+    CUSTOMERS_TABLE_BODY = (By.CSS_SELECTOR, "tbody")
+    FIRST_NAME_CELL = (By.CSS_SELECTOR, "tbody tr td:nth-child(1)")
+    LAST_NAME_CELL = (By.CSS_SELECTOR, "tbody tr td:nth-child(2)")
+    POST_CODE_CELL = (By.CSS_SELECTOR, "tbody tr td:nth-child(3)")
+
+class ManagerPage(BasePage):
+    @allure.step("Нажать на кнопку 'Add Customer'")
+    def click_add_customer_button(self):
+        self.find_element(ManagerPageLocators.ADD_CUSTOMER_BUTTON).click()
+
+    @allure.step("Заполнить форму клиента: Имя={first_name}, Фамилия={last_name}, Индекс={post_code}")
+    def fill_customer_form(self, first_name, last_name, post_code):
+        self.find_element(ManagerPageLocators.FIRST_NAME_INPUT).send_keys(first_name)
+        self.find_element(ManagerPageLocators.LAST_NAME_INPUT).send_keys(last_name)
+        self.find_element(ManagerPageLocators.POST_CODE_INPUT).send_keys(post_code)
+        self.find_element(ManagerPageLocators.SUBMIT_CUSTOMER_BUTTON).click()
+
+    @allure.step("Принять alert после добавления клиента")
+    def accept_alert(self):
+        alert = self.driver.switch_to.alert
+        alert_text = alert.text
+        alert.accept()
+        return alert_text
+
+    @allure.step("Перейти на вкладку 'Customers'")
+    def go_to_customers_tab(self):
+        self.find_element(ManagerPageLocators.CUSTOMERS_BUTTON).click()
+
+    @allure.step("Найти клиента по имени: {first_name}")
+    def find_customer_by_name(self, first_name):
+        self.find_element(ManagerPageLocators.SEARCH_INPUT).send_keys(first_name)
+        # Ожидаем, что таблица загрузится
+        self.find_element(ManagerPageLocators.CUSTOMERS_TABLE_BODY)
+
+    @allure.step("Проверить, что данные клиента в таблице соответствуют ожидаемым")
+    def verify_customer_data_in_table(self, first_name, last_name, post_code):
+        first_name_cell_text = self.find_element(ManagerPageLocators.FIRST_NAME_CELL).text
+        last_name_cell_text = self.find_element(ManagerPageLocators.LAST_NAME_CELL).text
+        post_code_cell_text = self.find_element(ManagerPageLocators.POST_CODE_CELL).text
+
+        with allure.step(f"Проверка имени: ожидаем '{first_name}', в таблице '{first_name_cell_text}'"):
+            assert first_name_cell_text == first_name
+        with allure.step(f"Проверка фамилии: ожидаем '{last_name}', в таблице '{last_name_cell_text}'"):
+            assert last_name_cell_text == last_name
+        with allure.step(f"Проверка индекса: ожидаем '{post_code}', в таблице '{post_code_cell_text}'"):
+            assert post_code_cell_text == post_code
--- tests/test_banking_flow.py
+++ tests/test_banking_flow.py
@@ -1,82 +1,33 @@
 import allure
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-from .utils import generate_customer_data
-
-
-def test_add_customer(driver, base_url):
-    """Тест-кейс TC-1: Успешное создание нового клиента."""
-    # Предусловие: URL из .env
-    driver.get(base_url)
-
-@allure.step("Нажать на кнопку 'Add Customer'")
-def step_click_add_customer_button(driver):
-    add_customer_btn = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.CSS_SELECTOR, "button[ng-click='addCust()']"))
-    )
-    add_customer_btn.click()
-
-@allure.step("Заполнить форму клиента: Имя={first_name}, Фамилия={last_name}, Индекс={post_code}")
-def step_fill_customer_form(driver, first_name, last_name, post_code):
-    first_name_input = WebDriverWait(driver, 10).until(
-        EC.visibility_of_element_located((By.CSS_SELECTOR, "input[ng-model='fName']"))
-    )
-    first_name_input.send_keys(first_name)
-    driver.find_element(By.CSS_SELECTOR, "input[ng-model='lName']").send_keys(last_name)
-    driver.find_element(By.CSS_SELECTOR, "input[ng-model='postCd']").send_keys(post_code)
-    driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
-
-@allure.step("Принять alert после добавления клиента")
-def step_accept_alert(driver):
-    alert = WebDriverWait(driver, 10).until(EC.alert_is_present())
-    alert_text = alert.text
-    alert.accept()
-    return alert_text
-
-@allure.step("Перейти на вкладку 'Customers' и найти клиента по имени: {first_name}")
-def step_find_customer_by_name(driver, first_name):
-    driver.find_element(By.CSS_SELECTOR, "button[ng-click='showCust()']").click()
-    search_input = WebDriverWait(driver, 10).until(
-        EC.visibility_of_element_located((By.CSS_SELECTOR, "input[ng-model='searchCustomer']"))
-    )
-    search_input.send_keys(first_name)
-
-@allure.step("Проверить, что данные клиента в таблице соответствуют ожидаемым")
-def step_verify_customer_data_in_table(driver, first_name, last_name, post_code):
-    first_name_cell = driver.find_element(By.CSS_SELECTOR, "tbody tr td:nth-child(1)")
-    last_name_cell = driver.find_element(By.CSS_SELECTOR, "tbody tr td:nth-child(2)")
-    post_code_cell = driver.find_element(By.CSS_SELECTOR, "tbody tr td:nth-child(3)")
-
-    with allure.step(f"Проверка имени: ожидаем '{first_name}', в таблице '{first_name_cell.text}'"):
-        assert first_name_cell.text == first_name
-    with allure.step(f"Проверка фамилии: ожидаем '{last_name}', в таблице '{last_name_cell.text}'"):
-        assert last_name_cell.text == last_name
-    with allure.step(f"Проверка индекса: ожидаем '{post_code}', в таблице '{post_code_cell.text}'"):
-        assert post_code_cell.text == post_code
+from pages.manager_page import ManagerPage
+from tests.utils import generate_customer_data
 
 @allure.title("TC-1: Успешное создание нового клиента")
-@allure.description("Проверка полного цикла создания нового клиента с динамическими данными и валидация его наличия в таблице.")
+@allure.description("Проверка полного цикла создания клиента с помощью Page Object.")
 @allure.feature("Клиенты")
 @allure.story("Создание клиента")
-@allure.severity(allure.severity_level.BLOCKER)
 def test_add_customer(driver, base_url):
+    # --- Инициализация Page Object ---
+    manager_page = ManagerPage(driver, base_url)
     
-    with allure.step("Предусловие: Открыть главную страницу"):
-        driver.get(base_url)
+    # --- Предусловие ---
+    with allure.step("Открыть главную страницу менеджера"):
+        manager_page.open()
 
-    step_click_add_customer_button(driver)
-
+    # --- Шаги теста ---
+    manager_page.click_add_customer_button()
+    
     first_name, post_code = generate_customer_data()
     last_name = "Testov"
     
-    step_fill_customer_form(driver, first_name, last_name, post_code)
+    manager_page.fill_customer_form(first_name, last_name, post_code)
     
-    alert_text = step_accept_alert(driver)
+    alert_text = manager_page.accept_alert()
     with allure.step("Проверка текста в alert"):
         assert "Customer added successfully" in alert_text
 
-    step_find_customer_by_name(driver, first_name)
+    manager_page.go_to_customers_tab()
+    manager_page.find_customer_by_name(first_name)
     
-    step_verify_customer_data_in_table(driver, first_name, last_name, post_code)
+    # --- Проверка результата ---
+    manager_page.verify_customer_data_in_table(first_name, last_name, post_code)
